d987272aa3291dd7ad3fbda0759d604f5cf67cc6
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-01-10 15:03:51 +0100

    feat: scaffolding

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Iscrizione Newsletter</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- /Bootstrap -->
+</head>
+
+<body class="p-4">
+    <header class="container text-center">
+        <h1>PHP Iscrizione Newsletter</h1>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
